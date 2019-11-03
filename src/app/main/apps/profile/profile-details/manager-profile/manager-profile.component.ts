47f218619b29f6811d95a0ebd79f8d2709352ff2
CheyHong <cheyhong108@gmail.com>
2019-11-04 03:27:55 +0700

    changed profile-details

---
+++ src/app/main/apps/profile/profile-details/manager-profile/manager-profile.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { FuseConfigService } from './node_modules/@fuse/services/config.service';
+
+@Component({
+    selector   : 'manager-profile',
+    templateUrl: './manager-profile.component.html',
+    styleUrls  : ['./manager-profile.component.scss']
+})
+export class ManagerProfileComponent
+{
+    /**
+     * Constructor
+     *
+     * @param {FuseConfigService} _fuseConfigService
+     */
+    constructor(
+        private _fuseConfigService: FuseConfigService,
+    )
+    {
+        // Configure the layout
+        this._fuseConfigService.config = {
+            layout: {
+                navbar   : {
+                    hidden: false
+                },
+                toolbar  : {
+                    hidden: false
+                },
+                footer   : {
+                    hidden: true
+                },
+                sidepanel: {
+                    hidden: true
+                }
+            }
+        };
+    }
+}
