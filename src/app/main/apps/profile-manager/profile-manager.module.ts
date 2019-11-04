b77dc6cb689ca675e381ae26df4557ea9f915a98
CheyHong <cheyhong108@gmail.com>
2019-11-04 21:01:21 +0700

    updated profile

--- src/app/main/apps/profile-manager/profile-manager.module.ts
+++ src/app/main/apps/profile-manager/profile-manager.module.ts
@@ -26,7 +26,7 @@ import { ProfileManagerPaymentComponent } from 'app/main/apps/profile-manager/pr
 
 const routes: Routes = [
     {
-        path  : 'index',
+        path  : 'manager',
         component: ProfileManagerComponent,
         resolve  : {
             provider: ProfileManagerService
@@ -34,7 +34,7 @@ const routes: Routes = [
     },
     {
         path      : '**',
-        redirectTo: 'index',
+        redirectTo: 'manager',
         pathMatch: 'full'
     }
 ];
