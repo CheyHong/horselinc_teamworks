6d914ece4b7da7b3ac09f06d43a8cfed17631ccb
CheyHong <cheyhong108@gmail.com>
2019-11-04 21:12:22 +0700

    changed payments

---
+++ src/app/main/apps/payment-provider/provider-dialogs/create-dialog/create-dialog.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+import { FuseSidebarService } from '@fuse/components/sidebar/sidebar.service';
+
+@Component({
+  selector: 'create-dialog',
+  templateUrl: './create-dialog.component.html',
+  styleUrls: ['./create-dialog.component.scss']
+})
+export class CreateDialogComponent implements OnInit {
+
+  constructor(
+    private _fuseSidebarService: FuseSidebarService
+  ) { }
+
+  ngOnInit() {
+  }
+
+  onShowDialog(){
+    this._fuseSidebarService.getSidebar('payment-provider-edit-panel').toggleOpen();
+  }
+
+}
